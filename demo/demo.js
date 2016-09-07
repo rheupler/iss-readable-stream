12738c152bb1d7f2095ab28d832791881da8cf84
ryan heupler <rheupler@gmail.com>
2016-09-06 21:07:41 -0700

    fixes some formatting issues and drys some functions up

--- demo/demo.js
+++ demo/demo.js
@@ -3,11 +3,11 @@ var DeltaStream = require('../lib/transform');
 var sampleStreamDelta = new DeltaStream(25544, 2);
 var sampleStreamLocation = new LocationStream(25544, 2);
 
-sampleStreamDelta.setEncoding('utf8');
+sampleStreamLocation.setEncoding('utf8');
 
-sampleStreamDelta.on('readable', function(){
+sampleStreamLocation.on('readable', function(){
 
-  var chunk = sampleStreamDelta.read();
+  var chunk = sampleStreamLocation.read();
   if (chunk !== null){
     console.log(chunk);
   }
